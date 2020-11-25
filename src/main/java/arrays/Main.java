bd5769e893b3aead40d2134ad5b2e882791d4704
Dmitri <dmitrikalvan@gmail.com>
2020-11-25 22:19:52 +0300

    Timed the execution time of the sorts of arrays

---
+++ src/main/java/arrays/Main.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int bigCapacity = 1_000_000;
+        MyArrayList<Integer> malForBubble = new MyArrayList<>(bigCapacity);
+        MyArrayList<Integer> malForSelection = new MyArrayList<>(bigCapacity);
+        MyArrayList<Integer> malForInsertion = new MyArrayList<>(bigCapacity);
+        for (int i = 0; i < bigCapacity; i++) {
+            malForBubble.add(rand.nextInt(bigCapacity));
+            malForSelection.add(rand.nextInt(bigCapacity));
+            malForInsertion.add(rand.nextInt(bigCapacity));
+        }
+
+        long bubbleSortTime = System.currentTimeMillis();
+        malForBubble.bubbleSort();
+        System.out.println("Bubble sort: " + (System.currentTimeMillis() - bubbleSortTime));
+
+        long selectionSortTime = System.currentTimeMillis();
+        malForSelection.selectionSort();
+        System.out.println("Selection sort: " + (System.currentTimeMillis() - selectionSortTime));
+
+        long insertionSortTime = System.currentTimeMillis();
+        malForInsertion.insertionSort();
+        System.out.println("Insertion sort: " + (System.currentTimeMillis() - insertionSortTime));
+    }
+}
