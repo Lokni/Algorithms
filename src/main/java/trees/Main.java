5fd35b4b28e0cfe315711694c82267aaac851eeb
Dmitri <dmitrikalvan@gmail.com>
2020-12-09 21:30:00 +0300

    Add class Home work with needed methods.

--- src/main/java/trees/Main.java
+++ src/main/java/trees/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         MyTreeMap<Integer, String> map = new MyTreeMap<Integer, String>();
 
-
+        HomeWork myHW = new HomeWork();
+        myHW.homework();
 //        map.put(5,"five");
 //        map.put(1,"one");
 //        map.put(2,"two");
