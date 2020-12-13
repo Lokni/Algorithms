aa8bef3396b5efb22c43a9150697de7375305f55
Dmitri <dmitrikalvan@gmail.com>
2020-12-13 13:03:40 +0300

    A bit changed class BreadthFirstPaths add method distTo and some tests for input data.

--- src/main/java/graph/BreadthFirstPaths.java
+++ src/main/java/graph/BreadthFirstPaths.java
@@ -5,12 +5,25 @@ import java.util.LinkedList;
 public class BreadthFirstPaths {
     private boolean[] marked;
     private int[] edgeTo;
+    private int[] distTo;
     private int source;
+    private int infinity = Integer.MAX_VALUE;
+
 
     public BreadthFirstPaths(Graph g, int source) {
+        if (source < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (source > g.getVertexCount() - 1) {
+            throw new IndexOutOfBoundsException();
+        }
         this.source = source;
         edgeTo = new int[g.getVertexCount()];
+        distTo = new int[g.getVertexCount()];
         marked = new boolean[g.getVertexCount()];
+        for (int i = 0; i < g.getVertexCount(); i++) {
+            distTo[i] = infinity;
+        }
         bfs(g, source);
     }
 
@@ -18,12 +31,15 @@ public class BreadthFirstPaths {
         LinkedList<Integer> queue = new LinkedList<>();
         queue.addLast(source);
         marked[source] = true;
+        distTo[source] = 0;
+
         while (!queue.isEmpty()) {
             int vertex = queue.removeFirst();
             for (int w : g.getAdjList(vertex)) {
                 if (!marked[w]) {
                     marked[w] = true;
                     edgeTo[w] = vertex;
+                    distTo[w] = distTo[vertex] + 1;
                     queue.addLast(w);
                 }
             }
@@ -32,6 +48,12 @@ public class BreadthFirstPaths {
 
 
     public boolean hasPathTo(int dist) {
+        if (dist < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (dist > marked.length) {
+            throw new IndexOutOfBoundsException();
+        }
         return marked[dist];
     }
 
@@ -46,5 +68,16 @@ public class BreadthFirstPaths {
             vertex = edgeTo[vertex];
         }
         return stack;
+    }
+
+    public int distTo(int dist) {
+        if (dist < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (dist > marked.length - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return distTo[dist];
     }
 }
